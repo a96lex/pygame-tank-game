d8389ccd8a2b401dcbb71fc9f999c96df6a8d211
a96lex <a96lex@gmail.com>
2022-07-12 20:01:04 +0200

    add quit helper

---
+++ game/helpers.py
@@ -0,0 +1,10 @@
+import pygame
+
+
+def check_quit_condition() -> None:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (
+            event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
+        ):
+            pygame.quit()
+            break
