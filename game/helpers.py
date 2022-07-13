d03bfa1b48e2e8fe1aac3304a00c44bf4f934012
a96lex <a96lex@gmail.com>
2022-07-13 17:11:46 +0200

    add circle collision checker

--- game/helpers.py
+++ game/helpers.py
@@ -1,8 +1,16 @@
 import pygame
 
+from .bullet import Bullet
+from .shooter import Shooter
+
 
 def check_quit_condition() -> None:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             break
+
+
+def cirlce_collision(c1: Shooter | Bullet, c2: Shooter | Bullet) -> bool:
+    distanceV = pygame.Vector2(c1.center - c2.center)
+    return distanceV.magnitude() < c1.radius + c2.radius, distanceV
