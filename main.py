47b51f75cc218788c49d618755c76ddca221ea82
a96lex <a96lex@gmail.com>
2022-07-13 17:34:36 +0200

    add enemies & level generation

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from random import random
 import pygame
 
 from game.bullet import Bullet
 from game.constants import Colors
-from game.helpers import check_quit_condition
+from game.helpers import check_quit_condition, cirlce_collision
+from game.levels import load_level
 from game.player import Player
 
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
@@ -10,6 +12,10 @@ pygame.display.init()
 pygame.display.set_caption("Tank game")
 
 player = Player(screen)
+
+lvl = 0
+enemies = load_level(lvl, player)
+
 bullets = pygame.sprite.Group()
 particles = pygame.sprite.Group()
 
@@ -21,8 +27,8 @@ while True:
 
     if key[pygame.K_SPACE] or pygame.mouse.get_pressed()[0]:
         if player.shooting_reload == 0:
+            player.shoot()
             bullets.add(Bullet(player))
-            player.shooting_reload = player.BulletStats.cooldown
     if key[pygame.K_UP] or key[pygame.K_w]:
         player.update_speed(pygame.Vector2(0, -1))
     if key[pygame.K_LEFT] or key[pygame.K_a]:
@@ -44,8 +50,31 @@ while True:
         if bullet.lifespan == 0:
             bullets.remove(bullet)
         else:
-            particles.add([Bullet(bullet) for _ in range(2)])
+            if random() > 0.9:
+                particles.add(Bullet(bullet))
+
+        if bullet.from_player:
+            for enemy in enemies:
+                collided, direction = cirlce_collision(enemy, bullet)
+                if collided:
+                    enemy.take_damage(bullet.damage, direction)
+                    bullets.remove(bullet)
+        else:
+            collided, direction = cirlce_collision(player, bullet)
+            if collided:
+                player.take_damage(bullet.damage, direction)
+                bullets.remove(bullet)
+
+    for enemy in enemies:
+        enemy.update()
+        if enemy.shooting_reload == 0:
+            enemy.shoot()
+            bullets.add(Bullet(enemy))
 
     player.update()
+
+    if not enemies:
+        lvl += 1
+        enemies = load_level(lvl, player)
 
     pygame.display.update()
