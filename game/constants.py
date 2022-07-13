02e43195fd2db82f374e2842c5e8b7cdf06ac53c
a96lex <a96lex@gmail.com>
2022-07-13 17:12:17 +0200

    add couple of colors

--- game/constants.py
+++ game/constants.py
@@ -2,6 +2,8 @@ class Colors:
     Background = (255, 214, 255)
     Player = (10, 30, 50)
     PlayerBullet = (60, 10, 40)
+    EnemyBullet = (10, 40, 60)
+    HealthBar = (255, 255, 255)
 
 
 SpriteWidth = 2
