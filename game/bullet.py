a0aa78f3930105a5f856026fc330e8b898501761
a96lex <a96lex@gmail.com>
2022-07-13 13:43:24 +0200

    update bullets

--- game/bullet.py
+++ game/bullet.py
@@ -1,9 +1,11 @@
 from random import random
+from typing import Tuple
 
 import pygame
 
-from .constants import Colors
+
 from .player import Player
+from .constants import SpriteWidth
 
 
 class Bullet(pygame.sprite.Sprite):
@@ -12,27 +14,59 @@ class Bullet(pygame.sprite.Sprite):
     speed: float
     radius: float
     lifespan: int
+    color: Tuple[int, int, int]
 
     center: pygame.Vector2
     direction: pygame.Vector2
 
-    def __init__(self, surface: pygame.Surface, player: Player) -> None:
+    from_player: bool
+    random_movement: bool
+
+    # Bullets can also spawn bullets (particles)
+    class BulletStats:
+        speed = 2
+        radius = 4
+        lifespan = 40
+        precision = 0
+        recoil = 2
+        cooldown = 0
+        color = None
+        random_movement = True
+
+    def __init__(self, shooter: Player) -> None:
         pygame.sprite.Sprite.__init__(self)
-        self.surface = surface
-        self.speed = player.BulletStats.speed
-        self.lifespan = player.BulletStats.lifespan
-        self.radius = player.BulletStats.radius
-        self.center = pygame.Vector2(player.center)
+        self.surface = shooter.surface
+
+        self.color = shooter.BulletStats.color or shooter.color
+        self.speed = shooter.BulletStats.speed
+        self.lifespan = shooter.BulletStats.lifespan
+        self.radius = shooter.BulletStats.radius
+
+        self.center = pygame.Vector2(shooter.center)
         self.direction = pygame.Vector2()
+        self.random_movement = shooter.BulletStats.random_movement
+
+        if hasattr(shooter, "angle"):
+            angle = shooter.angle
+        else:
+            angle = random() * 360
+
         self.direction.from_polar(
-            (1, player.angle + (random() - 0.5) * player.BulletStats.precision)
+            (1, angle + (random() - 0.5) * shooter.BulletStats.precision)
         )
-        player.update_speed(self.direction, -player.BulletStats.recoil)
+
+        if hasattr(shooter, "update_speed"):
+            shooter.update_speed(self.direction, -shooter.BulletStats.recoil)
 
     def draw(self) -> None:
-        pygame.draw.circle(self.surface, Colors.Bullet, self.center, self.radius)
+        pygame.draw.circle(
+            self.surface, self.color, self.center, self.radius, SpriteWidth
+        )
 
     def update(self) -> None:
         self.lifespan -= 1
         self.center += self.speed * self.direction
+        if self.random_movement:
+            self.center.x += random() * self.speed
+            self.center.y += random() * self.speed
         self.draw()
