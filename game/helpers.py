75c11a14199cffea78bf7156477027e1aa4f1322
a96lex <a96lex@gmail.com>
2022-07-13 13:42:54 +0200

    dont exit on escape

--- game/helpers.py
+++ game/helpers.py
@@ -3,8 +3,6 @@ import pygame
 
 def check_quit_condition() -> None:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT or (
-            event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
-        ):
+        if event.type == pygame.QUIT:
             pygame.quit()
             break
