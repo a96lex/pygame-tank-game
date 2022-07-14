74bd10b203a6ad9456d17769433760f0f6c8e4a1
a96lex <a96lex@gmail.com>
2022-07-14 13:30:31 +0200

    update bullet.py
    removed placeholder properties and added explosion class

--- game/bullet.py
+++ game/bullet.py
@@ -7,23 +7,10 @@ from .player import Player
 from .shooter import Shooter
 
 
-from .constants import SpriteWidth
+from .constants import Colors, SpriteWidth
 
 
 class Bullet(pygame.sprite.Sprite):
-    surface: pygame.Surface
-
-    speed: float
-    radius: float
-    lifespan: int
-    color: Tuple[int, int, int]
-
-    center: pygame.Vector2
-    direction: pygame.Vector2
-
-    from_player: bool
-    random_movement: bool
-
     # Bullets can also spawn bullets (particles)
     class BulletStats:
         speed = 2
@@ -93,4 +80,25 @@ class Bullet(pygame.sprite.Sprite):
         if self.random_movement:
             self.center.x += random() * self.speed
             self.center.y += random() * self.speed
+        self.draw()
+
+
+class Explosion(Bullet):
+    color = Colors.UI
+
+    def __init__(self, shooter: Shooter) -> None:
+        pygame.sprite.Sprite.__init__(self)
+        self.radius = shooter.radius
+        self.surface = shooter.surface
+        self.center = shooter.center
+        self.lifespan = 10
+
+    def update(self) -> None:
+        self.lifespan -= 1
+        self.radius *= 1.05
+
+        if self.lifespan == 0:
+            self.kill()
+
+        self.radius += 1
         self.draw()
