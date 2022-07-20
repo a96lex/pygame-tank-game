11949d3d490422f85a9dcf24daec2fe329520f92
a96lex <a96lex@gmail.com>
2022-07-20 18:41:42 +0200

    update bg color to white

--- game/constants.py
+++ game/constants.py
@@ -1,7 +1,7 @@
 class Colors:
     Background = (166, 205, 255)
     Player = (37, 43, 44)
-    UI = (255, 240, 240)
+    UI = (255, 255, 255)
     PlayerBullet = Player
     EnemyBullet = Player
     HealthBar = UI
